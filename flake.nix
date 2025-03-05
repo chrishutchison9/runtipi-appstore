a4cb51eadf49f59c063144819e7afc20e80c9cbe
Chris Hutchison <christopher.hutchison@9kit.com>
2025-03-05 10:08:41 -0600

    feat: add flake configuration and add direnv support

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "DevShell for Runtipi with Nix Flakes and Direnv";
+
+  # Use a compatible nixpkgs version
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";  # Choose a compatible nixpkgs version for your project
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  # Define outputs (what gets built)
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-darwin"] (system: let
+      # Define the devShell
+      pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.git
+            pkgs.nodejs
+            pkgs.yarn
+          ];
+        };
+      });
+}
